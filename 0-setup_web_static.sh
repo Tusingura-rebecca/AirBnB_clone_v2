a6d2c5569acc19ba475ff99ecb21c25b3b2855f3
leroysb <leroybuliro@gmail.com>
2023-04-09 17:58:10 +0300

    reedit

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -30,15 +30,22 @@ SERVER_CONFIG=\
 server {
         listen 80 default_server;
         listen [::]:80 default_server;
+        add_header X-Served-By ${hostname};
         root /var/www/html;
-        index index.html index.htm index.nginx-debian.html;
+        index index.html index.htm;
+
         server_name _;
 
         location /hbnb_static/ {
                 alias /data/web_static/current/;
         }
-        location / {
-                try_files \$uri \$uri/ =404;
+        location /redirect_me {
+                return 301 https://www.alphaziro.tech/;
+        }
+        error_page 404 /404.html;
+        location /404 {
+                root /etc/nginx/html;
+                internal;
         }
 }"
 
